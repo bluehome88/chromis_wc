5a69044f8c32a37ae34c1def1aca565a4df1204f
Blue Webmaster <bluehome88@outlook.com>
2022-02-18 12:10:55 -0800

    rtf template

--- lib/MakeRTF.php
+++ lib/MakeRTF.php
@@ -15,7 +15,7 @@ function generateRTF( $s_file_name, $a_patient, $a_medical_cert, $a_user ){
 
 	// Heading Section
 	$sect = $rtf->addSection();
-	$sect->writeText('<br><br>Certificate of <br>capacity/ certificate of fitness<br>', new PHPRtfLite_Font(25, $font_name));
+	$sect->writeText('<br><br>Certificate of capacity/certificate of fitness<br>', new PHPRtfLite_Font(25, $font_name));
 
 	$sect->writeText('For use with workers compensation and Compulsory Third Party (CTP) motor accident injury claims.<br>', new PHPRtfLite_Font(13, $font_name));
 
@@ -69,23 +69,55 @@ function generateRTF( $s_file_name, $a_patient, $a_medical_cert, $a_user ){
 	$sect->writeText('<tab><tab>Driving ability<br><br>', new PHPRtfLite_Font(10, $font_name));
 	$sect->writeText('Other (please specify) eg psychological considerations, keep wound clean and dry<br><br>', new PHPRtfLite_Font(10, $font_name));
 	$sect->writeText('Comments<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->insertPageBreak();
+	
 
-	$sect->writeText('Capacity for Work', new PHPRtfLite_Font(12, $font_name));
+	$sect->writeText('<br><br>Capacity for Work', new PHPRtfLite_Font(12, $font_name));
 	$sect->writeText('(please consider the health benefits of good work when completing this section).<br>', new PHPRtfLite_Font(10, $font_name));
 	$sect->writeText('Where the word ‘capacity’ appears below it should be read as ‘fitness for work’ when the certificate is completed in a motor accident injury claim.<br>', new PHPRtfLite_Font(10, $font_name));
 	$sect->writeText('Do you require a copy of the position description/work duties?<br>', new PHPRtfLite_Font(10, $font_name));
 	$sect->writeText('<tab>Yes', new PHPRtfLite_Font(10, $font_name));
-	$sect->writeText('<tab>No', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('<tab>No<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('is fit for pre-injury work from<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('has capacity for some type of work from<tab>to<tab> for<tab> hours/day <tab> days/week<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('has no current capacity for any work from<tab> to <tab><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('If no current work capacity, estimated time to return to any type of employment<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Factors delaying recovery<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Do you recommend referral to workplace rehabilitation provider?<tab>Yes<tab>No<br><br>', new PHPRtfLite_Font(10, $font_name));
 
+	$sect->writeText('TREATING MEDICAL PRACTITIONER DETAILS<br>', new PHPRtfLite_Font(12, $font_name));
+	$sect->writeText('I certify that I am the nominated treating doctor or treating specialist (please tick) and I have examined this patient. The information and medical opinions contained in this certificate of capacity are, to the best of my knowledge, true and correct.<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Signature<tab>Date(DD/MM/YYYY)<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Name<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Address<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Suburb<tab>State<tab>Postcode<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Telephone Number<tab>Provide Number<br><br>', new PHPRtfLite_Font(10, $font_name));
 
+	$checkbox = $sect->addCheckbox();
+	$sect->writeText('I agree to be the nominated treating doctor for the ongoing management of this person’s injury, treatment and recovery at/return to work (tick if you consent).<br><br>', new PHPRtfLite_Font(10, $font_name));
 
+	// Section 3
+	$sect->writeText('Section 3: Employment declaration', new PHPRtfLite_Font(14, $font_name));
+	$sect->writeText('(not to be completed by the treating medical practitioner)<br><br>', new PHPRtfLite_Font(9, $font_name));
+	$sect->writeText('This section is to be completed by the person prior to sending to the insurer (or employer).<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('First Name<tab>LastName<br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('<tab>I have<tab>I have not(tick appropriate box)<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('engaged in any form of paid employment, self employment or voluntary work for which I have received or am entitled to receive payment in money or otherwise since the last certificate was provided, that I have not yet declared to the insurer.<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('If so, please provide details below.<br><br><br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('I declare that the details I have given on this declaration are true and correct, knowing that false declarations are punishable by law.<br>', new PHPRtfLite_Font(10, $font_name));
+	$sect->writeText('Signature <tab>Date (DD/MM/YYYY)<br><br><br>', new PHPRtfLite_Font(10, $font_name));
 
-
+	// Footer
+	$sect->writeText('Catalogue No. SIRA08719<br>', new PHPRtfLite_Font(7, $font_name));
+	$sect->writeText('State Insurance Regulatory Authority, 92–100 Donnison Street, Gosford, NSW 2250 Locked Bag <br>', new PHPRtfLite_Font(7, $font_name));
+	$sect->writeText('2906, Lisarow, NSW 2252 | Customer Experience 13 10 50<br>', new PHPRtfLite_Font(7, $font_name));
+	$sect->writeText('Website www.sira.nsw.gov.au<br>', new PHPRtfLite_Font(7, $font_name));
+	$sect->writeText('© Copyright State Insurance Regulatory Authority 0318<br>', new PHPRtfLite_Font(7, $font_name));
+	
 
 	$rtf->save($output_file);
 
-	// downloadRTF( $s_file_name );
-echo $output_file;
+	downloadRTF( $s_file_name );
 	exit;
 }
 
